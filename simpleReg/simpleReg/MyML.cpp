356ad01be656f089133a59abaeca0b6366b9101d
1051823277 <1051823277@qq.com>
2020-03-05 21:38:38 +0800

    一个记录cost的变量没清零所以显示一直增长，一个计算diff_temp的时候不能直接用predict函数，再有增加迭代次数。

--- simpleReg/simpleReg/MyML.cpp
+++ simpleReg/simpleReg/MyML.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <math.h>
 
 using namespace std;
 //const int max_r = 500;
@@ -72,22 +73,25 @@ using namespace std;
 //}
 
 
+
+
+
 vector<double> predict(vector<double> theta, vector<vector<double>> x)
 {
 	int s = x.size();
 	vector<double> p;
 	for (int i = 0; i < s; i++) {
-		p.emplace_back(theta[0] * x[i][0] + theta[1] * x[i][1] + theta[2] * x[i][2]);
+		p.emplace_back(theta[0] * x[i][0]);
 	}
-
+	//cout << p.size();
 	return p;
 }
 
 vector<vector<double>> transpose(vector<vector<double>> x)
 {
 	vector<vector<double>> temp;
-	temp.reserve(15);
-	temp.resize(3, vector<double>(5));
+	temp.reserve(5);
+	temp.resize(1, vector<double>(5));
 	for (int i = 0; i < x.size(); i++) {
 		for (int j = 0; j < x[0].size(); j++) {
 			temp[j][i] = x[i][j];
@@ -100,33 +104,49 @@ vector<double> matminus(vector<double> x, vector<double> y)
 {
 	vector<double> temp;
 	for (int i = 0; i < x.size(); i++) {
-		temp.emplace_back(x.at(i) - y.at(i));
+		temp.emplace_back((x.at(i) - y.at(i)));
 	}
 	return temp;
 }
 
 
+
 int main()
 {
-	vector<vector<double>> x{ {0.5,1,1.5},{2,2.5,3},{3.5,4,4.5},{5,5.5,6},{6.5,7,7.5} };
-	vector<double> y{ 1, 2, 3, 4, 5 };
-	vector<double> theta{ 1,1,1 };
-	int iteration = 10;
-	int res = 0;
-	double alpha = 0.0005;
+	vector<vector<double>> x{ {1},{2},{3},{4},{5} };
+	vector<double> y{ 2, 4, 6, 8, 10 };
+	vector<double> theta{ 1};
+	int iteration = 100;
+	double res = 0;
+	double alpha = 0.005;
 
 	for (int iter = 0; iter < iteration; iter++) {
-		vector<double> temp = matminus(predict(theta, x), y);
+		vector<double> temp;
+		temp = matminus(predict(theta, x), y);
+		
 		for (int i = 0; i < temp.size(); i++) {
-			res += temp.at(i) * temp.at(i);
+			res += (temp.at(i) * temp.at(i));
 		}
-		double cost = res / 10;
+		
+		double cost = res / (2*x.size());
 		cout << "iter:" << iter << " " << "cost:" << cost << endl;
+		res = 0;
+		
+		vector<double> diff_temp{ 0 };
+		vector<double> xt{1,2,3,4,5};
+		
+		for (int i = 0; i < xt.size(); i++) {
+			diff_temp[0] += xt[i] * temp[i];
+		}
 
-		vector<double> diff_temp = predict(temp, transpose(x));
+		//cout << diff_temp[0];
+		
+		//cout << " "<<diff_temp.size()<<endl;
 		for (int i = 0; i < diff_temp.size(); i++) {
 			diff_temp.at(i) *= alpha;
+			diff_temp.at(i) /= x.size();
 			theta.at(i) -= diff_temp.at(i);
+			//cout << theta.at(i);
 		}	
 	}
 
